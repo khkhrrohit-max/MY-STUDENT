a217dc46396b5b930c56e37753ba688e24d123e7
khkhrrohit-max <khkhrrohit@gmail.com>
2026-07-23 19:11:05 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -54,7 +54,7 @@ sem1: [
     ],
 
     sem3: [
-        {name:"PDF 7", file:"pdf7.pdf"},
+         {name:"SIGNALS AND SYSTEMS (OTHER COLLEGE)", file:"https://drive.google.com/file/d/1v3Z2yTTE0-nClLWWWgNdmNgpQTRoy5Q6/view?usp=sharing"},
         {name:"PDF 8", file:"pdf8.pdf"},
         {name:"PDF 9", file:"pdf9.pdf"}
     ],
