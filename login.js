135fa628bfe91c2f31dede5e83501b363dd88759
khkhrrohit-max <khkhrrohit@gmail.com>
2026-07-24 20:11:19 +0530

    Update login.js

--- login.js
+++ login.js
@@ -21,3 +21,67 @@ function login(){
 
     window.location.href = "home.html";
 }
+function forgotPassword() {
+
+    const email = document.getElementById("email").value;
+
+    if (email === "") {
+        alert("Enter your email first");
+        return;
+    }
+
+    const data = localStorage.getItem(email);
+
+    if (!data) {
+        alert("Account Not Found");
+        return;
+    }
+
+    const user = JSON.parse(data);
+
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+
+    emailjs.send(
+        "service_50dbe0g",
+        "template_z26tf2w",
+        {
+            email: email,
+            otp: otp
+        }
+    )
+    .then(function () {
+
+        const enteredOTP = prompt("Enter OTP sent to your email");
+
+        if (enteredOTP === otp) {
+
+            const newPassword = prompt("Enter New Password");
+
+            if (newPassword && newPassword.trim() !== "") {
+
+                user.password = newPassword;
+
+                localStorage.setItem(email, JSON.stringify(user));
+
+                alert("Password Changed Successfully");
+
+            } else {
+
+                alert("Password cannot be empty");
+
+            }
+
+        } else {
+
+            alert("Wrong OTP");
+
+        }
+
+    })
+    .catch(function () {
+
+        alert("Failed to send OTP");
+
+    });
+
+}
