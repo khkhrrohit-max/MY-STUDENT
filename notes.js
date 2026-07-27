5394d61ff3b7c2da715d4bb76fab79fc984880c8
khkhrrohit-max <khkhrrohit@gmail.com>
2026-07-27 19:37:27 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -56,7 +56,11 @@ sem1: [
     sem3: [
          {name:"SIGNALS AND SYSTEMS (OTHER COLLEGE)", file:"https://drive.google.com/file/d/1v3Z2yTTE0-nClLWWWgNdmNgpQTRoy5Q6/view?usp=sharing"},
         {name:"HTML Notes", file:"https://drive.google.com/file/d/10y76MAiYDOkI6Frv10R2SfLWHZbXyVDd/view?usp=drivesdk"},
-        {name:"PDF 9", file:"pdf9.pdf"}
+        {name:"COA (Sec B)", file:"https://drive.google.com/file/d/1l-Oombq5oos4xGTMzLMGNjbWq8k8tidj/view?usp=sharing"},
+        {name:"MATH Notes", file:"https://drive.google.com/file/d/1nlwVyzxYjFNt4Umkm-7NiSq79rbV2-aj/view?usp=sharing"},
+        {name:"OOPS Notes", file:"https://drive.google.com/file/d/1S9Gtyzow7XdFpBxB7u_nSUpoKPYD6phZ/view?usp=sharing"},
+        {name:"SIGNALS ANS SYSTEMS", file:"https://drive.google.com/file/d/1oHN3QmkxZxGzBx9WV7FxuAoT2XDJdhey/view?usp=sharing"},
+        {name:"DESIGN AND ANALYSIS OF ALGORITHMS", file:"https://drive.google.com/file/d/1r426XzvlYwFG5Qh4ygrrMXTBAwH2vAMj/view?usp=sharing"}
     ],
 
     sem4: [
