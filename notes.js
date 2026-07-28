841d318e870082e2aa457f9c17aaf680413ce1ad
khkhrrohit-max <khkhrrohit@gmail.com>
2026-07-28 10:13:04 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -60,7 +60,9 @@ sem1: [
         {name:"MATH Notes", file:"https://drive.google.com/file/d/1nlwVyzxYjFNt4Umkm-7NiSq79rbV2-aj/view?usp=sharing"},
         {name:"OOPS Notes", file:"https://drive.google.com/file/d/1S9Gtyzow7XdFpBxB7u_nSUpoKPYD6phZ/view?usp=sharing"},
         {name:"SIGNALS ANS SYSTEMS", file:"https://drive.google.com/file/d/1oHN3QmkxZxGzBx9WV7FxuAoT2XDJdhey/view?usp=sharing"},
-        {name:"DESIGN AND ANALYSIS OF ALGORITHMS", file:"https://drive.google.com/file/d/1r426XzvlYwFG5Qh4ygrrMXTBAwH2vAMj/view?usp=sharing"}
+        {name:"DESIGN AND ANALYSIS OF ALGORITHMS", file:"https://drive.google.com/file/d/1r426XzvlYwFG5Qh4ygrrMXTBAwH2vAMj/view?usp=sharing"},
+            {name:"DAA (teacher)", file:"https://drive.google.com/file/d/1r426XzvlYwFG5Qh4ygrrMXTBAwH2vAMj/view?usp=sharing"}
+    
     ],
 
     sem4: [
