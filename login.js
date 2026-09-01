865c7f4e9dcb809b10f8a7e822dfec7bda7ef211
khkhrrohit-max <khkhrrohit@gmail.com>
2026-09-01 11:53:17 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,14 +3,14 @@ function login(){
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const data = localStorage.getItem(email);
+    let users = JSON.parse(localStorage.getItem("MY_STUDENT_USERS")) || {};
 
-    if(!data){
-        alert("Account Not Found");
-        return;
-    }
+const user = users[email];
 
-    const user = JSON.parse(data);
+if (!user) {
+    alert("Account Not Found");
+    return;
+}
 
     if(user.password !== password){
         alert("Wrong Password");
